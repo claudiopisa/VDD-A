2e4afbf239bbe64236c588ed4d4993f873613189
claudiopisa <claudiopisa9884@gmail.com>
2026-02-24 20:50:48 +0100

    update

--- src/DataReader/data_reader_task_versioning_internal.py
+++ src/DataReader/data_reader_task_versioning_internal.py
@@ -32,27 +32,15 @@ class DataReaderTaskVersioningInternal(DataReader):
         loader = section.get(self.config.sys_task.LOADER, " ")
         kernel = section.get(self.config.sys_task.KERNEL, " ")
         kernel_version = section.get(self.config.sys_task.KERNEL_VERSION, " ")
-
+    
         self.tasks.append(Task(name=boot, type="SYSTEM", version="<TODO>"))
         self.tasks.append(Task(name=boot_ap, type="SYSTEM", version="<TODO>"))
         self.tasks.append(Task(name=loader, type="SYSTEM", version="<TODO>"))
         self.tasks.append(Task(name=kernel, type="SYSTEM", version=kernel_version), modified="NO" if (self.prev_imgconf and self.prev_config.get(self.config.sys_task.KERNEL_VERSION) == kernel_version) else "YES")
 
     def _read_app_tasks(self, section: dict[str, str]):
-        # find all keys that start with "FileTask" and "RelTask"
-        file_task_pattern = re.compile(rf"^{self.config.app_task.path}\d*$")  # matches FileTask, FileTask1, FileTask2, etc.
-        rel_task_pattern = re.compile(rf"^{self.config.app_task.version}\d*$")  # matches RelTask, RelTask1, RelTask2, etc.
-
-        file_tasks = {key: value for key, value in section.items() if file_task_pattern.match(key)}
-        rel_tasks = {key: value for key, value in section.items() if rel_task_pattern.match(key)}
-
-        for file_key, file_name in file_tasks.items():
-            # Derive the corresponding version key by replacing the prefix
-            version_key = file_key.replace(self.config.app_task.path, self.config.app_task.version)
-            version = rel_tasks.get(version_key, "<TODO>")  # Get version or use placeholder if not found
-
-            self.tasks.append(Task(name=file_name, type="APPLICATION", version=version), modified="NO" if (self.prev_imgconf and self.prev_config.get(version_key) == version) else "YES")
-
+        # TODO
+        return 
 
     def scan_files(self) -> TaskList:
         # load current config
@@ -61,4 +49,5 @@ class DataReaderTaskVersioningInternal(DataReader):
 
         self._read_sys_tasks(section)
         self._read_app_tasks(section)
+
         return self.tasks   
