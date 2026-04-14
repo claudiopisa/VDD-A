7d52562616c29315ebc7a8fdef8c59250451bc93
claudiopisa <claudiopisa9884@gmail.com>
2026-04-14 22:18:47 +0200

    laptop commit

--- src/file_versioning_parser.py
+++ src/file_versioning_parser.py
@@ -5,55 +5,27 @@ from data_reader import FileVersioning
 from configs import CoreConfig, FileVersioningConfig
 from utils import setup_logging, get_logger
 from model import File
-
-root = ET.Element("FileVersioning")
-
-# Paragraph principale
-paragraph = ET.SubElement(root, "paragraph",
-    number="2",
-    title="LIST OF WSPHS+ SW FILES AND RELEVANT VERSIONS"
-)
-
-# Dummy values for testing without scan_files().
-# Expected shape: dict[folder_path, list[File]]
-out: dict[str, list[File]] = {
-    r"C:\Users\cpisa\Desktop\stream\DEVRASTA\NSPC\SWPG\INCLUDE": [
-        File(name="basdef16.h", version="1.2"),
-        File(name="basedef.h", version="3.2"),
-        File(name="bitmask.h", version="1.1"),
-    ],
-    r"C:\Users\cpisa\Desktop\stream\DEVRASTA\NSPC\SWPG\INC_PROT": [
-        File(name="APLDiag.h", version="1.3"),
-        File(name="Cdb.h", version="1.9"),
-        File(name="ParametersSRL.h", version=None),
-    ],
-}
-
-for idx, (folder, files) in enumerate(out.items(), start=1):
-    subparagraph = ET.SubElement(paragraph, "subparagraph", number=f"2.{idx}", title=folder)
-    table = ET.SubElement(subparagraph, "table")
-    for file in files:
-        ET.SubElement(
-            table,
-            "task",
-            name=file.name,
-            version=str(file.version),
-        )
-
-tree = ET.ElementTree(root)
-ET.indent(tree, space="  ")
-tree.write("file_versioning.xml", encoding="utf-8", xml_declaration=True)
-
+from parsers import FileVersioningParser
 
 setup_logging(level = "DEBUG")
 logger = get_logger(__name__)
 
-#core_conf = CoreConfig(user_config_path="config/core_config.json")
-#fv_conf = FileVersioningConfig(user_config_path="config/file_versioning.json", core_user_config_path=core_conf)
+core_conf = CoreConfig(user_config_path="config/core_config.json")
+fv_conf = FileVersioningConfig(user_config_path="config/file_versioning.json", core_user_config_path=core_conf)
 
-#reader = FileVersioning(config=fv_conf)
+reader = FileVersioning(config=fv_conf)
 
-#out = reader.scan_files() # output is FileCollection object
+out = reader.scan_files() # output is FileCollection object
 
-#print(out)
+print(out)
+
+parser = FileVersioningParser(root_name="FileVersioning",
+                              paragraph_title=fv_conf.title,
+                              paragraph_number=2,
+                              file_collection=out)
+
+
+parser.parse()
+parser.save("file_versioning_output.xml")
+
 
